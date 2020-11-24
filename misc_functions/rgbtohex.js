e8832718c5eb6ed7bf29a94c5a878459afeb0a38
EmilyRosaria <ebonywoodhellscythe@gmail.com>
2020-11-24 15:44:43 +0000

    recovered fun functions

---
+++ misc_functions/rgbtohex.js
@@ -0,0 +1,10 @@
+module.exports = {
+  execute(r,g,b) {
+    function componentToHex(c) {
+      var hex = Math.floor(c).toString(16);
+      return hex.length == 1 ? "0" + hex : hex;
+    }
+
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  },
+};

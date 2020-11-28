d567a5c6a2c22a83ba981eb5fb10331f03841fb3
Emily Rose <ebonywoodhellscythe@gmail.com>
2020-11-28 22:26:17 +0000

    personal update

--- commands/fun/copypasta.js
+++ commands/fun/copypasta.js
@@ -14,7 +14,7 @@ module.exports = {
     const numMatch = Number([args.join(' ').match(/\d+/) || 1][0]);
     let postCount = (numMatch > 5) ? 5 : numMatch;
     if (message.author.id == dev) {postCount = numMatch};
-    const nsfw = (message.channel.type == 'text') ? ((message.channel.nsfw && (!args.includes('safe') || !args.includes('sfw'))) || (message.author.id == dev)) : ((message.channel.type == 'dm') && args.includes('nsfw'));
+    const nsfw = (message.channel.type == 'text') ? ((message.channel.nsfw && (!args.includes('safe') || !args.includes('sfw'))) || (message.author.id == dev || message.guild.id == '749880737712308274')) : ((message.channel.type == 'dm') && args.includes('nsfw'));
     const rating = nsfw ? 'NSFW posts are enabled.' : 'All posts should be (relatively) SFW.';
     const plural = (postCount==1) ? 'post' : 'posts';
     message.reply('Fetching '+postCount.toString()+' '+plural+' from r/copypasta/top?t='+timeframe+'. '+rating);

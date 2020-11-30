d33eeb9ddc511218057423e54ea7522672d74b3e
Emily Rose <ebonywoodhellscythe@gmail.com>
2020-11-30 02:49:49 +0000

    Bug fixes and tweaks.

--- commands/dev/addlurkers.js
+++ commands/dev/addlurkers.js
@@ -5,27 +5,31 @@ module.exports = {
   aliases: ['lurkeradd'],
   description: 'Add members without a role to the lurker list!', // The description of the command (for help text)
   perms: 'dev', //restricts to bot dev only (me)
+  args: 1,
   usage: '<lurkerIDs>', // Help text to explain how to use the command (if it had any arguments)
   async execute(message, args) {
     var client = message.client;
     const guild = message.guild;
     const guildConfig = await GuildData.findById(message.guild.id).exec();
-    if (!guildConfig || !guildConfig.perms || typeof guildConfig.perms.purge == "undefined" || guildConfig.perms.purge < 0) {return message.reply("This command is only valid on guilds with the `purge` setting configured.")}
+    if (!guildConfig || !guildConfig.perms || typeof guildConfig.perms.purge == "undefined" || guildConfig.perms.purge < 0) {message.reply("This command is only valid on guilds with the `purge` setting configured."); return;}
     const roleChecks = guildConfig.perms.basic.concat(guildConfig.perms.advanced).concat(guildConfig.perms.spectator);
     const toDuration = require('./../../misc_functions/toDuration.js');
     let unFound = []; // IDs of users we couldn't fetch
     let fetched = []; // Lurker objects of users we could find
     let noLurk = []; // IDs of users who aren't lurkers
+    let left = []; // IDs of users who left the discord
     const cache = guild.members.cache; // bot's cache
-    fetched = args.filter(arg=>arg.match(/d{10,}/)).map(arg=>{
-      const id = arg.match(/d{10,}/)[0];
+    let parsed = [];
+    fetched = args.filter(arg=>arg.match(/\d{10,}/)).map(arg=>{
+      const id = arg.match(/\d{10,}/)[0];
+      parsed.push(id);
       if (guild.members.cache.has(id)) {
         const m = guild.members.cache.get(id);
-        if (roleChecks.filter(r=>m.roles.cache.has(r.id)).length == 0) {
+        if (roleChecks.filter(r=>m.roles.cache.has(r.id)).length > 0) {
           noLurk.push(id);
           return {_id: "0"};
         } else if (m.deleted) {
-          unFound.push(id);
+          left.push(id);
           return {_id: "0"};
         }
         let temp = {};
@@ -39,21 +43,45 @@ module.exports = {
         return {_id: "0"};
       }
     }).filter(m=>m._id != "0");
-
+    //message.reply("Cache checked... now fetching member data...");
+    //message.reply("Parsed: `"+parsed.join(' ')+"`");
     unFound = unFound.filter(async (id) => {
-      m = await guild.member(id).catch((err)=>{console.error(err); return false;});
+      m = await guild.member(id) //.then((g)=>g).catch((err)=>{console.error(err); return false;});
+      if (m && !m.deleted) {
+        if (roleChecks.filter(r=>m.roles.cache.has(r.id)).length == 0) {
+          let temp = {};
+          temp._id = m.id;
+          temp.joinedAt = m.joinedAt.getTime();
+          temp.warnings = 0;
+          temp.lastPing = m.joinedAt.getTime();
+          noLurk.push(temp);
+          return false;
+        } else {
+          fetched.push(temp);
+          return false;
+        }
+      } else if (m && m.deleted) {
+        left.push(id);
+        return false;
+      } else {
+        return true;
+      }
+    });
+    const lastFetch = await guild.members.fetch({ user: unFound, force: true}).then((members)=>members.map(m=>{
       if (m && !m.deleted) {
         let temp = {};
         temp._id = m.id;
         temp.joinedAt = m.joinedAt.getTime();
         temp.warnings = 0;
         temp.lastPing = m.joinedAt.getTime();
-        fetched.push(temp);
-        return false;
+        return temp;
       } else {
-        return true;
+        left.push(m.id);
+        return false;
       }
-    });
+    })).catch((err)=>{console.error(err); return [];});
+    unFound = unFound.filter(u=>!(lastFetch && lastFetch.find(f=>f._id == u)));
+    fetched = !!lastFetch ? fetched.concat(lastFetch) : fetched;
     if (unFound.length > 0) {
       message.reply("Could not fetch member data for the users pinged below.\n>>> "+unFound.map(id => `<@${id}>`).join('\n'));
     }
@@ -65,33 +93,6 @@ module.exports = {
     }
     let dupe = [];
     let added = [];
-    const newConfig = await fetched.reduce(async (acc, lurk) => {
-      let newGuildConfig = {};
-      if (acc.lurkers && acc.lurkers.length > 0) {
-        const oldLurker = acc.lurkers.find(L=>L._id == lurk._id);
-        if (oldLurker) {
-          dupe.push(lurk._id);
-          newGuildConfig = acc;
-        } else {
-          added.push(lurk._id);
-          newGuildConfig = await GuildData.findByIdAndUpdate(acc._id,{ "$push": { lurkers: lurk}},{new: true}).exec();
-        }
-      } else if (acc.lurkers && acc.lurkers.length == 0) {
-        added.push(lurk._id);
-        newGuildConfig = await GuildData.findByIdAndUpdate(acc._id,{ "$push": { lurkers: lurk}},{new: true}).exec();
-      } else {
-        added.push(lurk._id);
-        newGuildConfig = await GuildData.findByIdAndUpdate(acc._id,{ "$set": {lurkers: [lurk]}},{new: true}).exec();
-      }
-      return newGuildConfig;
-    },guildConfig);
-    if (dupe.length > 0) {
-      message.reply("The following users were already registered as lurkers:\n>>> "+dupe.map(id => `<@${id}>`).join('\n'));
-    }
-    if (added.length > 0) {
-      message.reply("The following users were added as lurkers:\n>>> "+added.map(id => `<@${id}>`).join('\n'));
-    } else {
-      message.reply("No valid users were found to be added as lurkers.");
-    }
+    newGuildConfig = await GuildData.findByIdAndUpdate(guild.id, {"$push": {lurkers: fetched}},{new: true})
   },
 };
---
+++ commands/dev/lurkers.js
@@ -0,0 +1,18 @@
+const GuildData = require('./../../database/models/guilds.js');
+
+module.exports = {
+  name: 'lurkers', // The name of the command
+  aliases: ['getlurkers'],
+  description: 'Add members without a role to the lurker list!', // The description of the command (for help text)
+  perms: 'dev', //restricts to bot dev only (me)
+  args: false,
+  usage: '', // Help text to explain how to use the command (if it had any arguments)
+  async execute(message, args) {
+    var client = message.client;
+    const guild = message.guild;
+    const guildConfig = await GuildData.findById(message.guild.id).exec();
+    if (!guildConfig || !guildConfig.lurkers || guildConfig.lurkers.length == 0) {message.reply("This guild has no logged lurkers."); return;}
+    const lurkers = guildConfig.lurkers.map(L=>`<@${L._id}>`);
+    message.reply("This guild has the following lurkers:\n>>> "+lurkers.join('\n'));
+  },
+};
